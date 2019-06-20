1ead703a6b9f21a0437f9e1123bd41af0ae458c5
xuehan <xuehan@ict.ac.cn>
2019-06-20 17:52:25 +0800

    sw done

--- landmark/sw.go
+++ landmark/sw.go
@@ -22,31 +22,101 @@ func (s *SW) getPaths (src, dest utils.RouterID) []utils.Path {
 		path := make(utils.Path, 1)
 		curr := src
 		path[0] = src
-
+		dir := UP
 		for {
-			if curr == dest {
+			next := s.getNextHop(destAddr, root, curr, dir)
+			path = append(path, next)
+			curr = next
+			if next == dest {
 				break
 			}
+			if next == root {
+				dir = DOWN
+			}
+		}
+		paths = append(paths, path)
+	}
+	return paths
+}
 
+func minPart(amount utils.Amount, mins []utils.Amount) []utils.Amount {
+	remainder := amount
+	saturated := make(map[int]struct{})
+	res := randomPartition(amount, len(mins))
 
-
-
+	if len(res) == 0 {
+		return nil
+	}
+	for remainder > 0 {
+		remainder = 0
+		for i := 0; i < len(res); i++ {
+			if res[i] > mins[i] {
+				remainder = remainder + res[i] - mins[i]
+				res[i] = mins[i]
+				saturated[i] = struct{}{}
+			}
 		}
 
-
-
-
-
-
+		if len(saturated) == len(mins) {
+			return nil
+		} else {
+			if remainder > 0 {
+				adds := randomPartition(remainder, len(mins)-len(saturated))
+				k := 0
+				for i := 0; i < len(adds); i++ {
+					for {
+						if _, ok := saturated[k]; ok {
+							k++
+						} else {
+							break
+						}
+					}
+					res[k] = res[k] + adds[i]
+					k++
+				}
+			}
+		}
 	}
-
-	return paths
+	return res
 }
 
 func (s *SW) SendPayment (src, dest utils.RouterID, amt utils.Amount) (
 	bool, error) {
 
-	return false, nil
+	paths := s.getPaths(src, dest)
+	caps := make([]utils.Amount, len(paths))
+	for _, path := range paths {
+		cap := utils.GetPathCap(path, s.Channels)
+		caps = append(caps, cap)
+	}
+
+	allcs := minPart(amt,caps)
+	if allcs == nil {
+		return false, nil
+	}
+
+	sentList := make([]utils.Amount, 0)
+	sentPaths := make([]utils.Path, 0)
+	for i, path := range paths {
+		if utils.GetPathCap(path, s.Channels) >= allcs[i] {
+			err := s.UpdateWeights(paths[i:i+1], allcs[i:i+1])
+			if err != nil {
+				return false, err
+			}
+			sentList = append(sentList, allcs[i])
+			sentPaths = append(sentPaths, path)
+		} else {
+			// 回滚
+			for j := range sentPaths {
+				err := s.UpdateWeightsReverse(sentPaths[j:j+1], sentList[j:j+1])
+				if err != nil {
+					return false, err
+				}
+			}
+			return false, nil
+		}
+	}
+	return true, nil
 }
 
 func (s *SW) getNextHop(dest string, root, current utils.RouterID,
