952b1162cd73004914de5d2456c4fc87ec3e2d45
xuehan <xuehan@ict.ac.cn>
2019-07-13 13:08:49 +0800

    before change array to map

---
+++ utils/graphproc.go
@@ -0,0 +1,17 @@
+package utils
+
+func (g *Graph)CutOneDegree()  {
+	for _, n := range g.Nodes {
+		if len(n.Neighbours) == 1 {
+			
+		}	
+	}
+}
+
+func (g *Graph)ConvertToSeriesID() *Graph {
+	return &Graph{}
+}
+
+func (g *Graph)()  {
+	
+}
